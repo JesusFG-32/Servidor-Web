db75645511d8143b4176ff6523997a8382904302
Jesus Antonio Flores Gonzalez <jesusfloresglz12@outlook.com>
2026-02-18 19:08:11 -0600

    Remover comentarios innecesarios

--- detector.go
+++ detector.go
@@ -6,64 +6,59 @@ import (
 
 func detectDetails(ua string) (os, browser, bgColor, textColor string) {
 	uaLower := strings.ToLower(ua)
-
-	// Default values
 	os = "Unknown"
 	browser = "Unknown"
-	bgColor = "#FFFFFF" // White
+	bgColor = "#FFFFFF"
 	textColor = "#000000"
 
-	// OS Detection & Color
 	if strings.Contains(uaLower, "windows") {
 		os = "Windows"
-		bgColor = "#0078D7" // Windows Blue
+		bgColor = "#0078D7"
 		textColor = "#FFFFFF"
 	} else if strings.Contains(uaLower, "mac os") || strings.Contains(uaLower, "macintosh") {
 		os = "macOS"
-		bgColor = "#F0F0F0" // Mac Light Gray
+		bgColor = "#F0F0F0"
 		textColor = "#000000"
 	} else if strings.Contains(uaLower, "android") {
 		os = "Android"
-		bgColor = "#3DDC84" // Android Green
+		bgColor = "#3DDC84"
 		textColor = "#000000"
 	} else if strings.Contains(uaLower, "iphone") || strings.Contains(uaLower, "ipad") {
 		os = "iOS"
-		bgColor = "#000000" // Black for iOS
+		bgColor = "#000000"
 		textColor = "#FFFFFF"
 	} else if strings.Contains(uaLower, "linux") {
-		// Specific Linux Distro check
 		if strings.Contains(uaLower, "ubuntu") {
 			os = "Ubuntu"
-			bgColor = "#E95420" // Ubuntu Orange
+			bgColor = "#E95420"
 			textColor = "#FFFFFF"
 		} else if strings.Contains(uaLower, "debian") {
 			os = "Debian"
-			bgColor = "#A80030" // Debian Red
+			bgColor = "#A80030"
 			textColor = "#FFFFFF"
 		} else if strings.Contains(uaLower, "fedora") {
 			os = "Fedora"
-			bgColor = "#294172" // Fedora Blue
+			bgColor = "#294172"
 			textColor = "#FFFFFF"
 		} else if strings.Contains(uaLower, "arch") {
 			os = "Arch Linux"
-			bgColor = "#1793D1" // Arch Blue/Cyan
+			bgColor = "#1793D1"
 			textColor = "#FFFFFF"
 		} else if strings.Contains(uaLower, "mint") {
 			os = "Linux Mint"
-			bgColor = "#87CF3E" // Mint Green
+			bgColor = "#87CF3E"
 			textColor = "#000000"
 		} else if strings.Contains(uaLower, "gentoo") {
 			os = "Gentoo"
-			bgColor = "#54487A" // Gentoo Purple
+			bgColor = "#54487A"
 			textColor = "#FFFFFF"
 		} else {
 			os = "Linux"
-			bgColor = "#333333" // Default Linux Dark Gray
+			bgColor = "#333333"
 			textColor = "#FFFFFF"
 		}
 	}
 
-	// Browser Detection (Simple heuristic)
 	if strings.Contains(uaLower, "firefox") {
 		browser = "Mozilla Firefox"
 	} else if strings.Contains(uaLower, "chrome") {
